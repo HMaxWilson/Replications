0d25e2d15d05aeddf95ab47bf0f9683bfe01e0ec
Harrison Max-Wilson <H.MaxWilson@hotmail.com>
2022-11-06 20:51:13 +0000

    Discord - Finish channel content sidebar

--- pages/discord.vue
+++ pages/discord.vue
@@ -78,11 +78,134 @@
       </div>
       <div class="flex-1 flex overflow-y-hidden">
         <div class="bg-gray-800 w-56 flex-none flex flex-col justify-between">
-          <div class="overflow-y-auto">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda cupiditate ducimus perferendis provident quos suscipit voluptatum. Assumenda corporis culpa dignissimos fugit, ipsa perspiciatis possimus tempore vero. Ad cupiditate ipsum, laboriosam obcaecati quis rerum? A ab, ad adipisci aliquam, animi asperiores atque blanditiis, commodi consequuntur dolor dolores eius eos est ex excepturi exercitationem facere hic minima modi natus numquam obcaecati omnis optio placeat provident quas quisquam ratione reiciendis repellat sequi similique vel velit veniam! Ad aliquid, architecto consectetur consequuntur facere fuga laboriosam nesciunt, quia quibusdam vel veniam voluptas. A alias aliquam consequatur corporis delectus deleniti explicabo harum ipsa magni, molestias non quasi quibusdam quisquam sapiente totam ullam vel veniam vitae voluptatem voluptatibus! Asperiores harum quas similique sint sunt. Accusantium autem beatae commodi cumque dignissimos doloribus dolorum eum facere facilis incidunt inventore labore maiores, molestias necessitatibus odio odit, officiis provident quasi recusandae similique soluta tempore tenetur! Hic magni natus officia quam quia reiciendis soluta! Atque dicta dolor esse facere impedit quisquam ratione! Aliquid culpa cumque debitis deleniti dignissimos dolores doloribus dolorum eaque eius error est ex facilis fugiat impedit iste iure laborum, libero magnam modi molestias nam non obcaecati provident quae quam, quod quos ratione rem repellat similique sunt tempora vitae voluptatem.
+          <div class="text-sm overflow-y-auto">
+            <ul class="px-2 py-3">
+              <li class="text-gray-500 px-2 hover:text-gray-200 hover:bg-gray-900">
+                <a href="#" class="flex items-center">
+                  <span class="text-xl">#</span>
+                  <span class="ml-2">welcome</span>
+                </a>
+              </li>
+              <li class="text-gray-500 px-2 hover:text-gray-200 hover:bg-gray-900">
+                <a href="#" class="flex items-center">
+                  <span class="text-xl">#</span>
+                  <span class="ml-2">faq</span>
+                </a>
+              </li>
+            </ul>
+
+            <button class="flex items-center text-gray-500 hover:text-gray-200 ml-1">
+              <svg class="w-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+              </svg>
+              <h3 class="uppercase tracking-wide font-semibold text-xs ml-1">Tailwind CSS</h3>
+            </button>
+            <ul class="px-2 py-3 pt-2">
+              <li class="text-gray-200 px-2 hover:text-gray-200 hover:bg-gray-900 bg-gray-750 rounded">
+                <a href="#" class="flex items-center">
+                  <span class="text-xl">#</span>
+                  <span class="ml-2">general</span>
+                </a>
+              </li>
+              <li class="text-gray-200 px-2 hover:text-gray-200 hover:bg-gray-900">
+                <a href="#" class="flex items-center">
+                  <span class="text-xl">#</span>
+                  <span class="ml-2">core-dev</span>
+                </a>
+              </li>
+              <li class="text-gray-500 px-2 hover:text-gray-200 hover:bg-gray-900">
+                <a href="#" class="flex items-center">
+                  <span class="text-xl">#</span>
+                  <span class="ml-2">course</span>
+                </a>
+              </li>
+              <li class="text-gray-500 px-2 hover:text-gray-200 hover:bg-gray-900">
+                <a href="#" class="flex items-center">
+                  <span class="text-xl">#</span>
+                  <span class="ml-2">plugins</span>
+                </a>
+              </li>
+              <li class="text-gray-200 px-2 hover:text-gray-200 hover:bg-gray-900">
+                <a href="#" class="flex items-center">
+                  <span class="text-xl">#</span>
+                  <span class="ml-2">docs</span>
+                </a>
+              </li>
+              <li class="text-gray-500 px-2 hover:text-gray-200 hover:bg-gray-900">
+                <a href="#" class="flex items-center">
+                  <span class="text-xl">#</span>
+                  <span class="ml-2">showcase</span>
+                </a>
+              </li>
+              <li class="text-gray-200 px-2 hover:text-gray-200 hover:bg-gray-900">
+                <a href="#" class="flex items-center">
+                  <span class="text-xl">#</span>
+                  <span class="ml-2">help</span>
+                </a>
+              </li>
+            </ul>
+
+            <button class="flex items-center text-gray-500 hover:text-gray-200 ml-1">
+              <svg class="w-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+              </svg>
+              <h3 class="uppercase tracking-wide font-semibold text-xs ml-1">Community</h3>
+            </button>
+            <ul class="px-2 py-3">
+              <li class="text-gray-200 px-2 hover:text-gray-200 hover:bg-gray-900">
+                <a href="#" class="flex items-center">
+                  <span class="text-xl">#</span>
+                  <span class="ml-2">feedback</span>
+                </a>
+              </li>
+              <li class="text-gray-500 px-2 hover:text-gray-200 hover:bg-gray-900">
+                <a href="#" class="flex items-center">
+                  <span class="text-xl">#</span>
+                  <span class="ml-2">jobs</span>
+                </a>
+              </li>
+            </ul>
+
+            <button class="flex items-center text-gray-500 hover:text-gray-200 ml-1">
+              <svg class="w-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+              </svg>
+              <h3 class="uppercase tracking-wide font-semibold text-xs ml-1">Off Topic</h3>
+            </button>
+            <ul class="px-2 py-3">
+              <li class="text-gray-500 px-2 hover:text-gray-200 hover:bg-gray-900">
+                <a href="#" class="flex items-center">
+                  <span class="text-xl">#</span>
+                  <span class="ml-2">design</span>
+                </a>
+              </li>
+              <li class="text-gray-500 px-2 hover:text-gray-200 hover:bg-gray-900">
+                <a href="#" class="flex items-center">
+                  <span class="text-xl">#</span>
+                  <span class="ml-2">development</span>
+                </a>
+              </li>
+              <li class="text-gray-200 px-2 hover:text-gray-200 hover:bg-gray-900">
+                <a href="#" class="flex items-center">
+                  <span class="text-xl">#</span>
+                  <span class="ml-2">random</span>
+                </a>
+              </li>
+            </ul>
           </div>
-          <div class="bg-gray-500 h-12">
-            Bottom
+          <div class="bg-gray-850 px-2 py-2 flex items-center justify-between">
+            <div class="flex items-center">
+              <a href="#"><img class="w-8 h-8 rounded-full" src="https://avatars.githubusercontent.com/u/33638862?s=40&amp;v=4" alt="@HMaxWilson" size="20" data-view-component="true"></a>
+              <div class="text-xs ml-2">
+                <div class="text-white">HMaxWilson</div>
+                <div class="text-gray-500 text-xxs">#1727</div>
+              </div>
+            </div>
+            <div class="flex items-center text-gray-500">
+              <a href="#"><svg aria-hidden="true" role="img" width="20" height="20" viewBox="0 0 24 24"><path fill-rule="evenodd" clip-rule="evenodd" d="M14.99 11C14.99 12.66 13.66 14 12 14C10.34 14 9 12.66 9 11V5C9 3.34 10.34 2 12 2C13.66 2 15 3.34 15 5L14.99 11ZM12 16.1C14.76 16.1 17.3 14 17.3 11H19C19 14.42 16.28 17.24 13 17.72V21H11V17.72C7.72 17.23 5 14.41 5 11H6.7C6.7 14 9.24 16.1 12 16.1ZM12 4C11.2 4 11 4.66667 11 5V11C11 11.3333 11.2 12 12 12C12.8 12 13 11.3333 13 11V5C13 4.66667 12.8 4 12 4Z" fill="currentColor"></path><path fill-rule="evenodd" clip-rule="evenodd" d="M14.99 11C14.99 12.66 13.66 14 12 14C10.34 14 9 12.66 9 11V5C9 3.34 10.34 2 12 2C13.66 2 15 3.34 15 5L14.99 11ZM12 16.1C14.76 16.1 17.3 14 17.3 11H19C19 14.42 16.28 17.24 13 17.72V22H11V17.72C7.72 17.23 5 14.41 5 11H6.7C6.7 14 9.24 16.1 12 16.1Z" fill="currentColor"></path></svg></a>
+              <a href="#" class="ml-3"><svg aria-hidden="true" role="img" width="20" height="20" viewBox="0 0 24 24"><svg width="24" height="24" viewBox="0 0 24 24"><path d="M12 2.00305C6.486 2.00305 2 6.48805 2 12.0031V20.0031C2 21.1071 2.895 22.0031 4 22.0031H6C7.104 22.0031 8 21.1071 8 20.0031V17.0031C8 15.8991 7.104 15.0031 6 15.0031H4V12.0031C4 7.59105 7.589 4.00305 12 4.00305C16.411 4.00305 20 7.59105 20 12.0031V15.0031H18C16.896 15.0031 16 15.8991 16 17.0031V20.0031C16 21.1071 16.896 22.0031 18 22.0031H20C21.104 22.0031 22 21.1071 22 20.0031V12.0031C22 6.48805 17.514 2.00305 12 2.00305Z" fill="currentColor"></path></svg></svg></a>
+              <a href="#" class="ml-3"><svg aria-hidden="true" role="img" width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" d="M19.738 10H22V14H19.739C19.498 14.931 19.1 15.798 18.565 16.564L20 18L18 20L16.565 18.564C15.797 19.099 14.932 19.498 14 19.738V22H10V19.738C9.069 19.498 8.203 19.099 7.436 18.564L6 20L4 18L5.436 16.564C4.901 15.799 4.502 14.932 4.262 14H2V10H4.262C4.502 9.068 4.9 8.202 5.436 7.436L4 6L6 4L7.436 5.436C8.202 4.9 9.068 4.502 10 4.262V2H14V4.261C14.932 4.502 15.797 4.9 16.565 5.435L18 3.999L20 5.999L18.564 7.436C19.099 8.202 19.498 9.069 19.738 10ZM12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16Z"></path></svg></a>
+            </div>
           </div>
         </div>
         <div class="flex-1 flex justify-between">
